ab8ac595e7f3dbc7b147d369de6949e53b8d4c2a
hannykee <hchw1904@gmail.com>
2018-02-05 16:26:51 +0900

    re

---
+++ 연습문제3.py
@@ -0,0 +1,13 @@
+#연습문제3 - 급여계산
+
+#함수호출
+def calc_monthly_salary():
+    pay_t= time * pay
+    print("시간당 급여는 %d 입니다." %pay_t)
+#변수
+
+#메인코드
+
+time=int(input("근무 시간을 입력하세요"))
+pay=int(input("시간당 급여를 입력하세요"))
+calc_monthly_salary()
