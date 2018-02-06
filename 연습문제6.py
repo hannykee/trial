26864d602691111f61a0c0a1e8fca91cb57cfa0d
hannykee <hchw1904@gmail.com>
2018-02-06 14:19:40 +0900

    re

---
+++ 연습문제6.py
@@ -0,0 +1,18 @@
+#연습문제6 암호화 프로그램
+
+
+#함수
+def block():
+
+
+#메인코드
+select=int(input(" 1.주민번호입력\n2.복호화\n3.종료"))
+
+if os==1:
+    a=input("주민번호를 입력하세요.")
+    list1=[]
+    for i in range(0,len(a)):
+        list1.append(a[i]^5)
+        print(list1)    
+        lock=a[i]
+    
---
+++ 연습문제8.py
@@ -0,0 +1,25 @@
+#연습문제 8 - 워터멜론
+
+def water_melon(n):
+    list10=""
+
+    
+    for i in range(1,n+1):
+        
+        if i % 2 == 0: #짝수 번호면 수 인쇄
+            list10+='박'
+        else : #홀수 번호면 박 인쇄
+            list10+='수'
+        
+    return(list10)
+
+
+#메인코드
+n=int(input("정수 n을 매개변수로 입력하세요."))
+
+nr=water_melon(n)
+n3=water_melon(3)
+n4=water_melon(4)
+print("입력한 n:", nr)
+print("n이 3인 경우: " ,n3 )
+print("n이 4인 경우: " ,n4 )
