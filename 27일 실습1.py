e30825ae00e3feb0b5e9d2c2c0a42946e16610fd
hannykee <hchw1904@gmail.com>
2018-02-13 23:24:13 +0900

    re

---
+++ 27일 실습1.py
@@ -0,0 +1,18 @@
+
+
+inStr="<<<파<<이>>썬>>>"
+outStr=""
+outStr1=""
+
+for i in range(0,len(inStr)) :
+    if inStr[i]!='<' :
+        outStr+=inStr[i]
+
+for i in range(0,len(outStr)) :
+    if outStr[i]!='>' :
+        outStr1+=outStr[i]
+    
+print('원 문자열 ==> '+'['+inStr + ']')
+print('공백 제거==>'+'[' + outStr1 + ']')
+
+
---
+++ 3번 문제.py
@@ -0,0 +1,7 @@
+#실습 문제 3 튜플
+
+tuple2= (1,2,3,4)
+tuple3 = (5,)
+
+tuple1=tuple2 + tuple3
+print(tuple1)
